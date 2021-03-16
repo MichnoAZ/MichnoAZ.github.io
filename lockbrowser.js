1f44adeb5775f1880b0e5986acb8a4cbfa76a3b8
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2021-03-16 12:37:52 +0500

    Create lockbrowser.js

---
+++ lockbrowser.js
@@ -0,0 +1,12 @@
+<script>
+function full() {
+document.documentElement.requestFullscreen()
+screen.orientation.lock("landscape")
+window.scrollTo(0,1)
+}
+function lock() {
+console.log('Fullscreen lock API by MichnoAZ version 0.1. status: 🔒 on')
+document.addEventListener('touchstart', full);
+}
+</script>
+<button onclick = "lock()">Lock down</button>
