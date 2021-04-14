d9916e88eabb1ff326bba00114b3794726edc276
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2021-04-14 12:09:55 +0500

    Update moapi.js

--- server/moapi.js
+++ server/moapi.js
@@ -3,4 +3,4 @@ document.write(`<meta name = "viewport" content = "width=device-width,initial-sc
 document.write(`<header><div id = "hi" style = "width:100%; background-color:#10de47; color:white; font-family:Arial; box-shadow:0 0 10px rgba(0,0,0,0.5); height:90px; position:fixed; top:0;">Mobile app</div></header><br>`);
 var ftext;
 document.write(`<div id = "content" style = " background-color:#c9c9c9; color:black; font-family:Arial; box-shadow:0 0 10px rgba(0,0,0,0.5); height:100%;">` + text + `</div>`);
-document.write(`<footer><div id = "foot" style = "width:100%; position:fixed; top:0; box-shadow:0 0 10px rgba(0,0,0,0.5); color:white; background-color:black; font-family:Arial;">` + ftext + `</div></footer>`);
+document.write(`<footer><div id = "foot" style = "width:100%; position:fixed; top:0; box-shadow:0 0 10px rgba(0,0,0,0.5); color:white; background-color:black; font-family:Arial; position: absolute; left: 0; bottom: 0;">` + ftext + `</div></footer>`);
