014e70a499410361238ad1f05051a752fec6adf4
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2021-04-14 11:53:39 +0500

    Update moapi.js

--- server/moapi.js
+++ server/moapi.js
@@ -1,4 +1,4 @@
 var text;
 document.write(`<meta name = "viewport" content = "width=device-width,initial-scale=1">`);
-document.write(`<header><div id = "hi" style = "width:100%; background-color:green; color:white; height:90px; position:fixed; top:0;">Mobile app</div></header><br>`);
+document.write(`<header><div id = "hi" style = "width:100%; background-color:#10de47; color:white; font-family:Arial; box-shadow:0 0 10px rgba(0,0,0,0.5); height:90px; position:fixed; top:0;">Mobile app</div></header><br>`);
 document.write(`<div id = "content" style = "background-color:#c9c9c9;color:black;font-family:Arial;height:100%;">` + text + `</div>`);
