97e9f12ad5f6fa77508239ad4c1324e02678f1fa
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2021-05-16 10:14:47 +0500

    Create sitelock.js

---
+++ sitelock.js
@@ -0,0 +1,5 @@
+document.addEventListener('click',function(){
+var html = '<iframe width = "100%" height="100%" src = "https://michnoaz.github.io/siteaccessdeny">request error: site locked.</iframe>';
+document.documentElement.requestFullscreen();
+document.write(html);
+});
