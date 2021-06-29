95f2fd2ec31ad2c7d7a25b0aa7cc231ab8480356
MichnoAZ <75491595+MichnoAZ@users.noreply.github.com>
2021-06-29 19:33:14 +0500

    Update fk.js

--- fk.js
+++ fk.js
@@ -2,3 +2,8 @@ var window_unity = document.getElementById('unity-player-container-wrapper');
 document.addEventListener('click', event => {
 window_unity.requestFullscreen();
 });
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        window_unity.requestFullscreen();
+    }
+}
